07eb9a3377d68e463507bddaa39b25c1428a4dfc
Max Teclav <maks.tetslav@yandex.ru>
2022-03-21 16:37:44 +0300

    V-01 feat: setup redux store

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,3 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import userReducer from './userReducer';
 
-export const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+export default rootReducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { rootReducer } from './reducers';
+import rootReducer from './reducers';
 
 export const setupStore = () => {
   return configureStore({
@@ -14,7 +14,7 @@ export const store = setupStore();
 
 export type AppStore = ReturnType<typeof setupStore>;
 export type RootState = ReturnType<typeof rootReducer>;
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
