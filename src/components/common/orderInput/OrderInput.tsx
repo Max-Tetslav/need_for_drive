aba156c971489daba2804981af1183908a4a0796
Max Teclav <maks.tetslav@yandex.ru>
2022-03-29 21:24:26 +0300

    F-03 feat: add dropdown and small improvements

--- src/components/common/orderInput/OrderInput.tsx
+++ src/components/common/orderInput/OrderInput.tsx
@@ -1,41 +1,45 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import classnames from 'classnames';
 import { Input } from 'antd';
 import clearIcon from '@assets/svg/clearIcon.svg';
+import OrderInputDropdown from '../orderInputDropdown/orderInputDropdown';
 import cl from './OrderInput.module.scss';
 
-interface IProps {
+interface IOrderInputProps {
   label: string;
-  id: string;
+  type: string;
   placeholder: string;
   value: string;
   setValue: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const OrderInput: React.FC<IProps> = ({ label, id, placeholder, value, setValue }) => {
+const OrderInput: React.FC<IOrderInputProps> = ({ label, type, placeholder, value, setValue }) => {
+  const [inputString, setInputString] = useState('');
+
   const changeHandler = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>): void => {
-      setValue(e.target.value);
+      setInputString(e.target.value);
     },
-    [value],
+    [inputString],
   );
 
   return (
-    <label htmlFor={`${id}-input`} className={cl.label}>
+    <label htmlFor={`${type}-input`} className={cl.label}>
       <span className={cl.labelText}>{label}</span>
       <Input
-        value={value}
+        value={inputString}
         onChange={changeHandler}
         bordered={false}
         className={cl.input}
         placeholder={placeholder}
-        id={`${id}-input`}
+        id={`${type}-input`}
         allowClear={{
           clearIcon: (
-            <img src={clearIcon} className={classnames(cl.clearIcon, { [cl.clearIconVisible]: value })} alt="clear-input" />
+            <img src={clearIcon} className={classnames(cl.clearIcon, { [cl.clearIconVisible]: inputString })} alt="clear-input" />
           ),
         }}
       />
+      <OrderInputDropdown setValue={setValue} value={inputString} type={type} />
     </label>
   );
 };
