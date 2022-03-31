b30b93a10df5218dd5d970366f181846ab873eb4
Max Teclav <maks.tetslav@yandex.ru>
2022-03-31 23:08:41 +0300

    F-03 feat: add reducers and setup in store

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import userReducer from './userReducer';
+import needForDriveApi from '@services/location';
+import locationReducer from './locationReducer';
 
 const rootReducer = combineReducers({
-  user: userReducer,
+  location: locationReducer,
+  [needForDriveApi.reducerPath]: needForDriveApi.reducer,
 });
 
 export default rootReducer;
