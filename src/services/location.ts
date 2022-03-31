73922f0ad7e0851c6bbaf981fa3e21420b4ac0b7
Max Teclav <maks.tetslav@yandex.ru>
2022-03-31 23:07:07 +0300

    F-03 feat: implement location api requests

---
+++ src/services/location.ts
@@ -0,0 +1,30 @@
+import { EndpointBuilder } from '@reduxjs/toolkit/dist/query/endpointDefinitions';
+import { BaseQueryFn, createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
+import { MAPBOX_REQUEST_HEADER_KEY } from '@utils/constants/locationData';
+
+const needForDriveApi = createApi({
+  reducerPath: 'locationApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api-factory.simbirsoft1.com/api/db',
+  }),
+  endpoints: (build: EndpointBuilder<BaseQueryFn, string, string>) => ({
+    getCitiesList: build.query({
+      query: () => ({
+        url: '/city',
+        headers: {
+          'X-Api-Factory-Application-Id': MAPBOX_REQUEST_HEADER_KEY,
+        },
+      }),
+    }),
+    getPointsList: build.query({
+      query: () => ({
+        url: '/point',
+        headers: {
+          'X-Api-Factory-Application-Id': MAPBOX_REQUEST_HEADER_KEY,
+        },
+      }),
+    }),
+  }),
+});
+
+export default needForDriveApi;
