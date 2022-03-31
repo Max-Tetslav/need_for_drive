a4b1ff2db22e5d606778531a96d2e100bf6355b8
Max Teclav <maks.tetslav@yandex.ru>
2022-03-31 23:24:04 +0300

    F-03 feat: implement interactions between map and data

---
+++ src/utils/helpers/locationHelpers.ts
@@ -0,0 +1,24 @@
+import { IGeoPoint } from '@models/mapData';
+import { GEOCODER_API } from '@utils/constants/locationData';
+import { GeoJSONSourceRaw, MapboxGeoJSONFeature } from 'mapbox-gl';
+
+export const getGeocoderUrl = (searchPoint: string) => {
+  return `https://geocode-maps.yandex.ru/1.x/?format=json&apikey=${GEOCODER_API}&geocode=${encodeURI(searchPoint)}}`;
+};
+
+export const getGeoJsonObject = (coordinateList: number[]): IGeoPoint => ({
+  type: 'Feature',
+  properties: {},
+  geometry: {
+    type: 'Point',
+    coordinates: coordinateList,
+  },
+});
+
+export const getGeoJsonObjectList = (pointsList: IGeoPoint[]): GeoJSONSourceRaw => ({
+  type: 'geojson',
+  data: {
+    type: 'FeatureCollection',
+    features: pointsList as MapboxGeoJSONFeature[],
+  },
+});
