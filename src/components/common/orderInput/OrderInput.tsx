01c4dd0bb65f5e438c8ae8e526219104a233f223
Max Teclav <maks.tetslav@yandex.ru>
2022-03-31 23:22:46 +0300

    F-03 feat: implement choice of city and point, interactions with data

--- src/components/common/orderInput/OrderInput.tsx
+++ src/components/common/orderInput/OrderInput.tsx
@@ -1,7 +1,12 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import classnames from 'classnames';
 import { Input } from 'antd';
 import clearIcon from '@assets/svg/clearIcon.svg';
+import needForDriveApi from '@services/location';
+import { useAppDispatch, useAppSelector } from '@store/store';
+import { updateCurrentCity } from '@store/reducers/locationReducer';
+import { DEFAULT_CITY } from '@utils/constants/locationData';
+import { ELocationInputTypes } from '@models/orderPageData';
 import OrderInputDropdown from '../orderInputDropdown/orderInputDropdown';
 import cl from './OrderInput.module.scss';
 
@@ -14,14 +19,54 @@ interface IOrderInputProps {
 }
 
 const OrderInput: React.FC<IOrderInputProps> = ({ label, type, placeholder, value, setValue }) => {
-  const [inputString, setInputString] = useState('');
+  const [inputString, setInputString] = type === ELocationInputTypes.CITY ? useState(DEFAULT_CITY) : useState('');
+  const [isFocus, setIsFocus] = useState(false);
+  const { data } =
+    type === ELocationInputTypes.CITY ? needForDriveApi.useGetCitiesListQuery('') : needForDriveApi.useGetPointsListQuery('');
+
+  const dispatch = useAppDispatch();
+  const currentCity = useAppSelector((state) => state.location.city);
 
   const changeHandler = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>): void => {
+      // Если input с городом пустой, сбрасывает все значения
+      if (e.target.value.length === 0) {
+        if (type === ELocationInputTypes.CITY) {
+          dispatch(updateCurrentCity(''));
+        }
+        setValue('');
+      }
+
       setInputString(e.target.value);
     },
     [inputString],
   );
+
+  const focusHandler = useCallback(() => {
+    setIsFocus(true);
+  }, []);
+
+  const blurHandler = useCallback(() => {
+    // Без таймаута не успевает сработать выбор варианта из списка
+    setTimeout(() => setIsFocus(false), 200);
+  }, []);
+
+  useEffect(() => {
+    // Если город становится пустым, пункт выдачи тоже
+    if (type === ELocationInputTypes.POINT && currentCity.length === 0) {
+      setValue('');
+      setInputString('');
+    }
+  }, [currentCity]);
+
+  useEffect(() => {}, [data, value, inputString]);
+
+  useEffect(() => {
+    // Устанавливает город по умолчанию
+    if (type === ELocationInputTypes.CITY) {
+      setValue(DEFAULT_CITY);
+    }
+  }, []);
 
   return (
     <label htmlFor={`${type}-input`} className={cl.label}>
@@ -29,9 +74,12 @@ const OrderInput: React.FC<IOrderInputProps> = ({ label, type, placeholder, valu
       <Input
         value={inputString}
         onChange={changeHandler}
+        onFocus={focusHandler}
+        onBlur={blurHandler}
         bordered={false}
         className={cl.input}
         placeholder={placeholder}
+        autoComplete="off"
         id={`${type}-input`}
         allowClear={{
           clearIcon: (
@@ -39,7 +87,14 @@ const OrderInput: React.FC<IOrderInputProps> = ({ label, type, placeholder, valu
           ),
         }}
       />
-      <OrderInputDropdown setValue={setValue} value={inputString} type={type} />
+      <OrderInputDropdown
+        type={type}
+        setValue={setValue}
+        isFocus={isFocus}
+        setString={setInputString}
+        inputString={inputString}
+        data={data}
+      />
     </label>
   );
 };
--- src/components/common/orderInputDropdown/orderInputDropdown.tsx
+++ src/components/common/orderInputDropdown/orderInputDropdown.tsx
@@ -1,98 +1,124 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import classNames from 'classnames';
+import { ELocationInputTypes, ICity, ILocationResponse, IPoint } from '@models/orderPageData';
+import { useAppDispatch, useAppSelector } from '@store/store';
+import { updateCurrentCity, updateCurrentPoint } from '@store/reducers/locationReducer';
 import cl from './orderInputDropdown.module.scss';
 
 interface IDropdownPlaceProps {
-  value: string;
+  inputString: string;
+  setString: React.Dispatch<React.SetStateAction<string>>;
   setValue: React.Dispatch<React.SetStateAction<string>>;
+  isFocus: boolean;
   type: string;
+  data: ILocationResponse;
 }
 
-const orderInputDropdown: React.FC<IDropdownPlaceProps> = ({ value, setValue, type }) => {
+const orderInputDropdown: React.FC<IDropdownPlaceProps> = ({ inputString, type, isFocus, setString, setValue, data }) => {
   const [isDropdownShown, setIsDropdownShown] = useState(false);
-  const [choice, setChoice] = useState('');
-  // const [data, setData] = useState<any[]>();
+  const [choice, setChoice] = type === ELocationInputTypes.CITY ? useState(true) : useState(false);
+  const dispatch = useAppDispatch();
+  const currentCity = useAppSelector((state) => state.location.city);
+
+  const [filteredData, setFilteredData] = useState<ICity[] | IPoint[]>(data ? data.data : []);
+
+  useEffect(() => {
+    setIsDropdownShown(isFocus);
+  }, [isFocus]);
+
+  useEffect(() => {
+    if (data) {
+      switch (type) {
+        case ELocationInputTypes.CITY:
+          // Фильтрует по строке
+          setFilteredData(
+            (data.data as ICity[]).filter((item: ICity) =>
+              item.name.toLocaleLowerCase().includes(inputString.toLocaleLowerCase()),
+            ),
+          );
+          break;
+        case ELocationInputTypes.POINT:
+          // Фильтрует по наличии города в адресе пункта выдачи, с выбранным городом, по строке
+          setFilteredData(
+            (data.data as IPoint[]).filter(
+              (item: IPoint) =>
+                Boolean(item.cityId) &&
+                item.cityId.name.toLocaleLowerCase() === currentCity.toLocaleLowerCase() &&
+                item.name.toLocaleLowerCase().includes(inputString.toLocaleLowerCase()),
+            ),
+          );
+          break;
+        default: {
+          if (filteredData.length === 0) {
+            setFilteredData(data.data);
+            dispatch(updateCurrentCity(''));
+          }
+        }
+      }
+    }
+  }, [inputString, data, currentCity]);
 
   const clickHandler = useCallback((e: React.MouseEvent<HTMLLIElement, MouseEvent>): void => {
-    setValue((e.target as HTMLLIElement).textContent as string);
-    setChoice((e.target as HTMLLIElement).textContent as string);
+    const selectedValue = (e.target as HTMLLIElement).textContent as string;
+    setChoice(true);
+
+    setValue(selectedValue);
+    setString(selectedValue);
+
+    switch (type) {
+      case ELocationInputTypes.CITY:
+        dispatch(updateCurrentCity(selectedValue));
+        break;
+      case ELocationInputTypes.POINT:
+        dispatch(updateCurrentPoint(selectedValue));
+        break;
+
+      // no default
+    }
+
     setIsDropdownShown(false);
   }, []);
 
   useEffect(() => {
-    if (choice) {
-      setIsDropdownShown(false);
-      return;
-    }
-
-    if (value.length && !isDropdownShown) {
+    // скрывает если выбран какой-то вариант
+    if (inputString.length && !choice) {
       setIsDropdownShown(true);
     }
 
-    if (value.length === 0) {
-      setIsDropdownShown(false);
+    if (inputString.length === 0) {
+      // setIsDropdownShown(false);
+      setChoice(false);
     }
-  }, [value]);
+  }, [inputString]);
+
+  let notFoundMessage;
+
+  if (type === ELocationInputTypes.POINT && !currentCity) {
+    // если город не выбран
+    notFoundMessage = <li className={cl.dropdownItem}>Сначала выберите город</li>;
+  } else if (inputString.length !== 0 && !choice) {
+    // если нет совпадений по строке
+    notFoundMessage = <li className={cl.dropdownItem}>Совпадений не найдено</li>;
+  } else if (filteredData.length === 0) {
+    // если в городе нет пунктов выдачи
+    notFoundMessage = <li className={cl.dropdownItem}>В этом городе нет пункта выдачи</li>;
+  }
 
   return (
     <ul className={classNames(cl.dropdown, { [cl.showDropdown]: isDropdownShown })}>
-      {/* {data!.map((item) => { */}
-      <li
-        className={cl.dropdownItem}
-        onClick={clickHandler}
-        onKeyUp={(e: React.KeyboardEvent) => false}
-        role="option"
-        aria-selected="false"
-      >
-        ВЫАЫВАФЫВАФ
-      </li>
-      <li
-        className={cl.dropdownItem}
-        onClick={clickHandler}
-        onKeyUp={(e: React.KeyboardEvent) => false}
-        role="option"
-        aria-selected="false"
-      >
-        ФЫВАЫВАФАВЫФА
-      </li>
-      <li
-        className={cl.dropdownItem}
-        onClick={clickHandler}
-        onKeyUp={(e: React.KeyboardEvent) => false}
-        role="option"
-        aria-selected="false"
-      >
-        ффвыафываывф
-      </li>
-      <li
-        className={cl.dropdownItem}
-        onClick={clickHandler}
-        onKeyUp={(e: React.KeyboardEvent) => false}
-        role="option"
-        aria-selected="false"
-      >
-        фвыаывфаыва
-      </li>
-      <li
-        className={cl.dropdownItem}
-        onClick={clickHandler}
-        onKeyUp={(e: React.KeyboardEvent) => false}
-        role="option"
-        aria-selected="false"
-      >
-        фвыаывфаыва
-      </li>
-      <li
-        className={cl.dropdownItem}
-        onClick={clickHandler}
-        onKeyUp={(e: React.KeyboardEvent) => false}
-        role="option"
-        aria-selected="false"
-      >
-        фвыаывфаыва
-      </li>
-
-      {/* })} */}
+      {notFoundMessage ||
+        (filteredData as Array<ICity | IPoint>).map((item) => (
+          <li
+            key={item.id}
+            className={cl.dropdownItem}
+            onClick={clickHandler}
+            onKeyUp={() => false}
+            role="option"
+            aria-selected="false"
+          >
+            {type === ELocationInputTypes.CITY ? item.name : (item as IPoint).address}
+          </li>
+        ))}
     </ul>
   );
 };
