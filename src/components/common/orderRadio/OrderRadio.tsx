db795ece7d17ec78d2baf0deff1b18cc1f5c604b
Max Teclav <maks.tetslav@yandex.ru>
2022-04-05 13:50:49 +0300

    V-03/F-04 feat: implement orderRadio component

---
+++ src/components/common/orderRadio/OrderRadio.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import cl from './OrderRadio.module.scss';
+
+interface IOrderRadioProps {
+  title: string;
+  checked: boolean;
+}
+
+const OrderRadio: React.FC<IOrderRadioProps> = ({ title, checked }) => {
+  return (
+    <label className={cl.label} htmlFor={`${title}-radio`}>
+      <input className={cl.input} type="radio" id={`${title}-radio`} name="model" checked={checked} />
+      <span className={cl.checkmark} />
+      {title}
+    </label>
+  );
+};
+
+export default OrderRadio;
