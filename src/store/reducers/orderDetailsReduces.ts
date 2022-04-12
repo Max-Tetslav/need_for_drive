7866a259a53bdcf73ab490447572d776bf55b22a
Max Teclav <maks.tetslav@yandex.ru>
2022-04-13 00:26:06 +0300

    V-04/F-05 feat: implement add actions for orderDetails

--- src/store/reducers/orderDetailsReduces.ts
+++ src/store/reducers/orderDetailsReduces.ts
@@ -5,7 +5,9 @@ export interface IOrderPointData {
   status: boolean;
   value: {
     city: string;
+    cityId: string;
     address: string;
+    addressId: string;
   };
 }
 
@@ -16,7 +18,22 @@ export interface IOrderModelData {
     model: string;
     maxPrice: number;
     minPrice: number;
+    colors: string[];
   };
+}
+
+export interface IOrderOptionsData {
+  color: string;
+  rateName: string;
+  rateId: string;
+  ratePrice: number;
+  dateFrom: number;
+  dateTo: number;
+  duration: string;
+  finalPrice: string;
+  isFullTank: boolean | null;
+  isNeedChildChair: boolean | null;
+  isRightWheel: boolean | null;
 }
 
 interface IOrderDetailsInitialState {
@@ -26,6 +43,7 @@ interface IOrderDetailsInitialState {
   };
   point: IOrderPointData;
   model: IOrderModelData;
+  options: IOrderOptionsData;
 }
 
 export const initialState: IOrderDetailsInitialState = {
@@ -37,7 +55,9 @@ export const initialState: IOrderDetailsInitialState = {
     status: false,
     value: {
       city: 'Ульяновск',
+      cityId: '',
       address: '',
+      addressId: '',
     },
   },
   model: {
@@ -47,7 +67,21 @@ export const initialState: IOrderDetailsInitialState = {
       model: '',
       maxPrice: 0,
       minPrice: 0,
+      colors: [],
     },
+  },
+  options: {
+    color: '',
+    rateName: '',
+    rateId: '',
+    ratePrice: 0,
+    dateFrom: 0,
+    dateTo: 0,
+    duration: '',
+    finalPrice: '',
+    isFullTank: null,
+    isNeedChildChair: null,
+    isRightWheel: null,
   },
 };
 
@@ -79,6 +113,42 @@ const orderDetailsSlice = createSlice({
     updateModelMinPrice(state, action: PayloadAction<number>) {
       state.model.value.minPrice = action.payload;
     },
+    updateModelColorsList(state, action: PayloadAction<string[]>) {
+      state.model.value.colors = action.payload;
+    },
+    updateColor(state, action: PayloadAction<string>) {
+      state.options.color = action.payload;
+    },
+    updateRateName(state, action: PayloadAction<string>) {
+      state.options.rateName = action.payload;
+    },
+    updateRateId(state, action: PayloadAction<string>) {
+      state.options.rateId = action.payload;
+    },
+    updateRatePrice(state, action: PayloadAction<number>) {
+      state.options.ratePrice = action.payload;
+    },
+    updateDateFrom(state, action: PayloadAction<number>) {
+      state.options.dateFrom = action.payload;
+    },
+    updateDateTo(state, action: PayloadAction<number>) {
+      state.options.dateTo = action.payload;
+    },
+    updateDuration(state, action: PayloadAction<string>) {
+      state.options.duration = action.payload;
+    },
+    updateFinalPrice(state, action: PayloadAction<string>) {
+      state.options.finalPrice = action.payload;
+    },
+    updateIsFullTank(state, action: PayloadAction<boolean>) {
+      state.options.isFullTank = action.payload;
+    },
+    updateIsNeedChildChair(state, action: PayloadAction<boolean>) {
+      state.options.isNeedChildChair = action.payload;
+    },
+    updateIsRightWheel(state, action: PayloadAction<boolean>) {
+      state.options.isRightWheel = action.payload;
+    },
   },
 });
 
@@ -91,5 +161,17 @@ export const {
   updateModelId,
   updateModelMaxPrice,
   updateModelMinPrice,
+  updateModelColorsList,
+  updateColor,
+  updateRateName,
+  updateRateId,
+  updateRatePrice,
+  updateDateFrom,
+  updateDateTo,
+  updateDuration,
+  updateFinalPrice,
+  updateIsFullTank,
+  updateIsNeedChildChair,
+  updateIsRightWheel,
 } = orderDetailsSlice.actions;
 export default orderDetailsSlice.reducer;
