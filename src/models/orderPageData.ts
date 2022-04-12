d88a138c107e04ad2580935290b6db312af93159
Max Teclav <maks.tetslav@yandex.ru>
2022-04-13 00:20:23 +0300

    V-04/F-05 feat: add orderDetails models

--- src/models/orderPageData.ts
+++ src/models/orderPageData.ts
@@ -10,6 +10,12 @@ export interface IBreadcrumbRoute {
 export enum ELocationInputTypes {
   CITY = 'city',
   POINT = 'point',
+  COLOR = 'color',
+  RATE = 'rate',
+  DURATION = 'duration',
+  TANK = 'tank',
+  CHAIR = 'chair',
+  WHEEL = 'wheel',
 }
 
 export interface ICity {
@@ -47,9 +53,28 @@ export interface ICar {
   categoryId: {
     id: string;
   };
+  colors: string[];
 }
 
 export enum EOrderItemTypes {
   POINT = 'point',
   MODEL = 'model',
+  COLOR = 'color',
+  DURATION = 'duration',
+  RATE = 'rate',
+  TANK = 'tank',
+  CHAIR = 'chair',
+  WHEEL = 'wheel',
+}
+
+export interface IRateTypeId {
+  id: string;
+  name: string;
+  unit: string;
+}
+
+export interface IRate {
+  id: string;
+  price: number;
+  rateTypeId?: IRateTypeId;
 }
