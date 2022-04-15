fb0f140013fc40a6356d6209ca4d1eb27e34f4ae
Max Teclav <maks.tetslav@yandex.ru>
2022-04-15 23:09:59 +0300

    V-06/F-02/F-07 refactor: remove useless reducers

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,13 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import needForDriveApi from '@services/needForDriveAPI';
-import breadcrumbReducer from './breadcrumbReducer';
-import locationReducer from './locationReducer';
 import orderDetailsReduces from './orderDetailsReduces';
 
 const rootReducer = combineReducers({
-  location: locationReducer,
   orderDetails: orderDetailsReduces,
-  breadcrumb: breadcrumbReducer,
   [needForDriveApi.reducerPath]: needForDriveApi.reducer,
 });
 
