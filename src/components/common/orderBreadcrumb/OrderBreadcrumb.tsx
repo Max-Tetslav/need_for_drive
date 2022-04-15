7da13aaab59f2b5c2b531e80a4ebc7bedb7e45c9
Max Teclav <maks.tetslav@yandex.ru>
2022-04-15 23:50:58 +0300

    V-06/F-02/F-07 feat: add orderNumber label after POST order; improve navigation

--- src/components/common/orderBreadcrumb/OrderBreadcrumb.tsx
+++ src/components/common/orderBreadcrumb/OrderBreadcrumb.tsx
@@ -1,32 +1,34 @@
-import React, { Fragment, useCallback, useEffect } from 'react';
+import React, { Fragment, useCallback, useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import classnames from 'classnames';
 import { Breadcrumb } from 'antd';
-import { useAppDispatch, useAppSelector } from '@store/store';
-import { updateCurrentModel } from '@store/reducers/breadcrumbReducer';
-import { EOrderItemTypes } from '@models/orderPageData';
+import { useAppSelector } from '@store/store';
 import breadcrumbRoutes from '@utils/constants/breadcrumbData';
 import separatorIcon from '@assets/svg/breadcrumbSeparator.svg';
 import cl from './OrderBreadcrumb.module.scss';
 
 const OrderBreadcrumb: React.FC = () => {
   const location = useLocation();
-  const dispatch = useAppDispatch();
   const orderDetailsData = useAppSelector((state) => state.orderDetails);
+  const [orderId, setOrderId] = useState('');
 
   const setIsReady = useCallback(
     (routeType: string): boolean => {
       let isReady = false;
       switch (routeType) {
         case 'point': {
-          isReady = Boolean(orderDetailsData.point.value.address);
+          isReady = true;
           break;
         }
         case 'model': {
-          isReady = Boolean(orderDetailsData.model.value.name);
+          isReady = Boolean(orderDetailsData.point.orderData.address);
           break;
         }
         case 'options': {
+          isReady = Boolean(orderDetailsData.model.value.name);
+          break;
+        }
+        case 'total': {
           isReady = Boolean(orderDetailsData.options.finalPrice);
           break;
         }
@@ -39,53 +41,64 @@ const OrderBreadcrumb: React.FC = () => {
   );
 
   useEffect(() => {
-    if (location.pathname.includes(EOrderItemTypes.MODEL)) {
-      dispatch(updateCurrentModel(true));
+    if (location.pathname.includes(':')) {
+      const pathArr = location.pathname.split('/');
+      const id = pathArr[pathArr.length - 1]
+        .replace(':', '')
+        .toLocaleUpperCase();
+
+      setOrderId(id);
+    } else {
+      setOrderId('');
     }
   }, [location]);
 
   return (
     <div className={cl.breadcrumbWrapper}>
-      <Breadcrumb separator="" className={cl.breadcrumb}>
-        {breadcrumbRoutes.map((route, index) => {
-          const isCurrentPath = location.pathname.includes(route.path);
-          const isStepComplete = setIsReady(route.type);
+      {orderId ? (
+        <p className={cl.orderId}>{`Заказ номер ${orderId}`}</p>
+      ) : (
+        <Breadcrumb separator="" className={cl.breadcrumb}>
+          {breadcrumbRoutes.map((route, index) => {
+            const isCurrentPath = location.pathname.includes(route.path);
+            const isStepReady = setIsReady(route.type);
 
-          return index === breadcrumbRoutes.length - 1 ? (
-            <Link
-              to={isStepComplete ? route.path : location.pathname}
-              key={route.id}
-              className={classnames(
-                cl.link,
-                { [cl.activeLink]: isCurrentPath },
-                { [cl.completeLink]: isStepComplete },
-              )}
-            >
-              {route.breadcrumbName}
-            </Link>
-          ) : (
-            <Fragment key={route.id}>
+            return index === breadcrumbRoutes.length - 1 ? (
               <Link
-                to={isStepComplete ? route.path : location.pathname}
+                to={isStepReady ? route.path : location.pathname}
+                key={route.id}
                 className={classnames(
                   cl.link,
                   { [cl.activeLink]: isCurrentPath },
-                  { [cl.completeLink]: isStepComplete },
+                  { [cl.completeLink]: isStepReady },
                 )}
               >
                 {route.breadcrumbName}
               </Link>
-              <Breadcrumb.Separator>
-                <img
-                  src={separatorIcon}
-                  className={cl.separator}
-                  alt="breadcrumb-separator"
-                />
-              </Breadcrumb.Separator>
-            </Fragment>
-          );
-        })}
-      </Breadcrumb>
+            ) : (
+              <Fragment key={route.id}>
+                <Link
+                  to={isStepReady ? route.path : location.pathname}
+                  className={classnames(
+                    cl.link,
+                    { [cl.activeLink]: isCurrentPath },
+                    { [cl.completeLink]: isStepReady },
+                  )}
+                >
+                  {route.breadcrumbName}
+                </Link>
+                <Breadcrumb.Separator>
+                  <img
+                    src={separatorIcon}
+                    className={cl.separator}
+                    alt="breadcrumb-separator"
+                  />
+                </Breadcrumb.Separator>
+              </Fragment>
+            );
+          })}
+        </Breadcrumb>
+      )}
     </div>
   );
 };
